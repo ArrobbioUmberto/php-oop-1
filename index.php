3175d31b3dee32af59ca4085000dbb4ddd8e0042
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-30 16:22:17 +0200

    FEAT multiple genre and actors

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ class Movie
     public string $title;
     public string $author;
     public string $duration;
-    public string $genre;
+    public $genre;
     public $actor;
     public int $releaseYear;
 
@@ -36,7 +36,7 @@ $film1->releaseYear = 1952;
 
 $film2 = new Movie("Il Signore degli Anelli - La Compagnia dell'Anello", "02:58:00");
 $film2->author = "Peter Jackson";
-$film2->genre = "Fantasy";
+$film2->genre = array("Fantasy", "Avventura", "Azione", "Epico", "Drammatico");
 $film2->actor = array("Elijah Wood", "Ian McKellen", "Liv Tyler", "Viggo Mortensen");
 $film2->releaseYear = 2001;
 ?>
@@ -58,19 +58,35 @@ $film2->releaseYear = 2001;
 <body>
     <div class="container py-4">
         <div class="row">
-            <div class="col py-2">
-                <h1> Titolo: <?php echo $film1->title ?></h1>
-                <h2> Regista: <?php echo $film1->author ?></h2>
-                <span> Genere/i: <?php echo $film1->genre ?> </span>
-                <p> Attori: <?php echo $film1->actor[0] ?></p>
-                <h3>Prima volta in sala <?php echo $film1->howOld() ?> anni fa'</h3>
+            <div class="card col">
+                <div class="card-body">
+                    <h5 class="card-title">Titolo: <?php echo $film1->title ?></h5>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Regista: <?php echo $film1->author ?></li>
+                    <li class="list-group-item">Genere/i: <?php echo json_encode($film1->genre); ?>
+
+                    </li>
+                    <li class="list-group-item">Attori: <?php foreach ($film1->actor as $value) {
+                                                            echo $value . ' ,';
+                                                        } ?></li>
+                    <li class="list-group-item">Prima volta in sala <?php echo $film1->howOld() ?> anni fa'</li>
+                </ul>
             </div>
-            <div class="col py-2">
-                <h1> Titolo: <?php echo $film2->title ?></h1>
-                <h2> Regista: <?php echo $film2->author ?></h2>
-                <span> Genere/i: <?php echo $film2->genre ?> </span>
-                <p> Attori: <?php echo $film2->actor[0] ?></p>
-                <h3>Prima volta in sala <?php echo $film2->howOld() ?> anni fa'</h3>
+            <div class="card col">
+                <div class="card-body">
+                    <h5 class="card-title">Titolo: <?php echo $film2->title ?></h5>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Regista: <?php echo $film2->author ?></li>
+                    <li class="list-group-item">Genere/i: <?php foreach ($film2->genre as $generi) {
+                                                                echo $generi . ' ,';
+                                                            }  ?> </li>
+                    <li class="list-group-item">Attori: <?php foreach ($film2->actor as $actors) {
+                                                            echo $actors . ' ,';
+                                                        } ?></li>
+                    <li class="list-group-item">Prima volta in sala <?php echo $film2->howOld() ?> anni fa'</li>
+                </ul>
             </div>
         </div>
     </div>
